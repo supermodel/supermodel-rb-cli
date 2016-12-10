b540d7d53272dbd2372d37fcc3b80415a9ccfa53
Z <zdenek@me.com>
2016-12-10 16:32:43 +0100

    Working example

--- lib/supermodel/cli.rb
+++ lib/supermodel/cli.rb
@@ -1,7 +1,7 @@
 require "supermodel/cli/version"
 
 module Supermodel
-  module CLI
+  class Cli
     def say_hello
       puts 'This is zerp. Coming in loud and clear. Over.'
     end
--- lib/supermodel/cli/version.rb
+++ lib/supermodel/cli/version.rb
@@ -1,5 +1,5 @@
 module Supermodel
-  module Cli
+  class Cli
     VERSION = "0.1.0"
   end
 end
